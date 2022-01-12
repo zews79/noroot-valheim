d305a88df3425d44ee2a177176d2001a9f6a423d
Jon <zews79@gmail.com>
2022-01-12 12:24:21 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # Downloading/updating server on startup
 
 echo "### Downloading Valheim Server ###"
-mkdir -p /opt/valheim/data
-/home/steam/steamcmd/linux32/steamcmd +login anonymous +force_install_dir /home/steam/valheim +app_update 896660 validate +quit
+mkdir -p /home/steam/valheim/data
+/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/valheim +app_update 896660 validate +quit
 
 exec "$@"
