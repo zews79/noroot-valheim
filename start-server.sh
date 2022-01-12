7755aefed20f8bfb0c8fc444e473c511426cbc12
Jon <zews79@gmail.com>
2022-01-12 11:42:24 -0800

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,14 +1,14 @@
 export temp_ldpath=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/valheim/linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/steam/valheim/linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
-cp /opt/valheim/linux64/steamclient.so /opt/valheim
+cp /opt/valheim/linux64/steamclient.so /home/steam/valheim
 
 export LD_LIBRARY_PATH=$temp_ldpath
 
 echo "### Starting Valheim server ###"
 while true; do
-/opt/valheim/valheim_server.x86_64 \
+/home/steam/valheim/valheim_server.x86_64 \
   -name ${SERVER_NAME} \
   -port ${SERVER_PORT} \
   -world ${SERVER_WORLD} \
